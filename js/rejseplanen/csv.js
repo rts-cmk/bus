3d6f1c9ec95d5d58b4010995c05f7732dc040a4b
Henrik Andersen <han@rts.dk>
2024-02-26 15:05:47 +0100

    add more debug logging

--- js/rejseplanen/csv.js
+++ js/rejseplanen/csv.js
@@ -8,13 +8,25 @@ export default class Csv extends EventTarget {
         if (!response.ok) throw new Error('Failed to load csv: ' + response.status + '\n' + response.statusText);
         let data = await response.arrayBuffer();
 
+        if (data.byteLength === 0) throw new Error('CSV: No data in file.');
+
         const decoder = new TextDecoder(options.encoding ?? 'utf-8');
 
+        console.log('CSV: decoding data with: ' + (options.encoding ?? 'utf-8'));
+
         data = decoder.decode(data);
+
+        if (data.length === 0) throw new Error('CSV: Decode failed.');
+
+        console.log('CSV: decoded data: ' + data);
+
+        if (options.regexReplace) console.log('CSV: replacing regex: ' + options.regexReplace);
 
         if (options.regexReplace) options.regexReplace.forEach(pair => data = data.replace(pair[0], pair[1]));
 
         const dataArray = data.split('\r\n').map( row => row.split(';') );
+
+        console.log('CSV: data array: ' + dataArray);
 
         if (options.skipRows) dataArray.splice(0, options.skipRows);
 
